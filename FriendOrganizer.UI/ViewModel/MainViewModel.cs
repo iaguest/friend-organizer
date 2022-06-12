7de669efa481f87c1968015757af53e70c1a15cf
I Guest <i.guest2010@gmail.com>
2022-06-12 12:39:28 +0100

    Implement MainViewModel and ViewModelBase

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IFriendDataService _friendDataService;
+        private Friend _selectedFriend;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            _friendDataService = friendDataService;
+            Friends = new ObservableCollection<Friend>();
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get
+            {
+                return _selectedFriend;
+            }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
